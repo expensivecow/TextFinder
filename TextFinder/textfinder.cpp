89139ee5917dd796b2fee6ec255ec2ad38f9546e
expensivecow <poppop208@gmail.com>
2014-11-15 18:13:09 -0800

    adding functional primitive string stream search

--- TextFinder/textfinder.cpp
+++ TextFinder/textfinder.cpp
@@ -1,8 +1,11 @@
 #include "textfinder.h"
 #include "ui_textfinder.h"
+#include <QtCore/QFile>
+#include <QtCore/QTextStream>
 
 TextFinder::TextFinder(QWidget *parent) : QMainWindow(parent), ui(new Ui::TextFinder) {
     ui->setupUi(this);
+    getTextFile();
 }
 
 TextFinder::~TextFinder() {
@@ -10,9 +13,19 @@ TextFinder::~TextFinder() {
 }
 
 void TextFinder::on_FindWord_clicked() {
-  //TODO
+  QString word = ui->lineEdit->text();
+  ui->textEdit->find(word, QTextDocument::FindWholeWords);
 }
 
 void TextFinder::getTextFile() {
-  //TODO
+  QFile myFile(":/inputFile.txt");
+  myFile.open(QIODevice::ReadOnly);
+
+  QTextStream textStream(&myFile);
+  QString line = textStream.readAll();
+  myFile.close();
+
+  ui->textEdit->setPlainText(line);
+  QTextCursor textCursor = ui->textEdit->textCursor();
+  textCursor.movePosition(QTextCursor::Start, QTextCursor::MoveAnchor, 1);
 }
